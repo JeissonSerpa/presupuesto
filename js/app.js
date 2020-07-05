6fda096960c2f0b36139ac94944aea2bc260fc29
Jeisson Serpa <jeisson-serpa@outlook.com>
2020-07-05 14:42:06 -0500

    Se agregan validaciones y alertas en caso de no llenar el formulario completo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Variables 
 const presupuestoUsuario = prompt('Cual es tu presupuesto semanal?');
+const formulario = document.querySelector('#agregar-gasto');
 let cantidadPresupuesto;
 
 
@@ -26,6 +27,26 @@ class Interfaz{
       restante.innerHTML = cantidad;
    }
 
+   imprimirMensaje(mensaje, tipo){
+      const divMensaje = document.createElement('div');
+      divMensaje.classList.add('text-center', 'alert');
+
+      if(tipo = 'error'){
+         divMensaje.classList.add('alert-danger');
+      }else{
+         divMensaje.classList.add('alert-success');
+      }
+
+      divMensaje.appendChild(document.createTextNode(mensaje));
+      //Insertar en el DOM
+      document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+      setTimeout(function(){
+         document.querySelector('.primario .alert').remove();
+         formulario.reset();
+      }, 2000)
+   }
+
 }
 
 
@@ -41,5 +62,20 @@ document.addEventListener('DOMContentLoaded', function(){
       //Instanciar la interfaz de usuario
       const ui = new Interfaz();
       ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
+   }
+});
+
+formulario.addEventListener('submit', function(e){
+   e.preventDefault();
+   const cantidad = document.querySelector('#cantidad').value;
+   const gasto = document.querySelector('#gasto').value;
+   const ui = new Interfaz();
+
+   if(cantidad === '' || gasto === ''){
+      ui.imprimirMensaje('Hubo un error', 'error');
+      
+   }else{
+      console.log(gasto);
+      console.log(cantidad);
    }
 });
